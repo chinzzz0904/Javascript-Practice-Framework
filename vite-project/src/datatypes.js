31c6b7a3daeb5940ce2b0360b0706e9eb8fc20b8
rakshitogra94 <rakshit@logica11y.com>
2025-12-12 19:15:19 -0500

    Initial commit for vite project

---
+++ vite-project/src/datatypes.js
@@ -0,0 +1,9 @@
+ //numnber => 2 to the power of 53
+ //bigint => for large numbers
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined => value not assigned
+ //symbol => unique
+
+ 
---
+++ vite-project/src/variables.js
@@ -0,0 +1,15 @@
+const accountId =  144553
+let accountEmail = "rakshit@google.com"
+var accountPassword = "12345"
+var accountCity = "Jammu & Kashmir"
+let accountState;
+
+// accountId = 2
+
+//console.log(accountId);
+
+accountEmail = "harishkumar@google.com"
+accountPassword = "21212121"
+accountCity = "Bangalore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
