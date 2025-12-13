4a5f8bc121f6472e56417e4d5cb77cef4a1eb853
rakshitogra94 <rakshit@logica11y.com>
2025-12-12 19:42:27 -0500

    Commit - Conversion Operations

---
+++ vite-project/src/conversionOps.js
@@ -0,0 +1,30 @@
+// let score = undefined
+
+// console.log(typeof score)
+
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+// let isLoggedIn = "rakshit"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+
+// "" => false
+// "rakshit" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
+
+console.log(stringNumber)
