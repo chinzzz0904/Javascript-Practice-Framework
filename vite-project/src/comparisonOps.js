05ae75a088ca8716b706debbb06edf90e28f0f9c
rakshitogra94 <rakshit@logica11y.com>
2025-12-24 19:47:31 -0500

    COmmit 24/12

---
+++ vite-project/src/comparisonOps.js
@@ -0,0 +1,16 @@
+// console.log(null > 0)
+
+// console.log(null == 0)
+
+// console.log(null <= 0)
+
+// console.log(undefined > 0)
+
+// console.log(undefined == 0)
+
+// console.log(undefined <= 0)
+
+// Try to avoid such comparisons
+
+console.log("2" === 2)
+
---
+++ vite-project/src/datatypes-summary.js
@@ -0,0 +1,23 @@
+//Primitive Data Types
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference Data Types
+
+// 3 types: Array, Object, Function
+
+// JavaScript is a dynamically typed language, which means that the type of a variable is determined by the value it holds at runtime.
+
+const bigNumber = 3456789012345678901234567890n;
+
+const hero = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {name: "Rakshit", age: 30}
+
+const myFunction = function(){
+  console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
