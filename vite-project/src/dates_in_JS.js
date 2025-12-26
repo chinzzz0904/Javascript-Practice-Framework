bb338308cc0f24adde798cfd68a85343e86a8c6a
rakshitogra94 <rakshit@logica11y.com>
2025-12-26 15:58:16 -0500

    Commit 26/12

---
+++ vite-project/src/dates_in_JS.js
@@ -0,0 +1,33 @@
+let myDate = new Date()
+
+// console.log(myDate)
+
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23,5,3)
+
+// console.log(myCreatedDate.toDateString())
+
+// console.log(myCreatedDate.toLocaleString())
+
+let myCreatedDate = new Date("2023-01-14")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp)
+
+// console.log(myCreatedDate.getTime())
+
+//console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date();
+
+console.log(newDate);
+
+console.log(newDate.getMonth()+1);
+
+console.log(newDate.getDay());
