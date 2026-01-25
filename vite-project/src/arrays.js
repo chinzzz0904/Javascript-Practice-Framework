19734cb149faa0569dd76d692672c69446e0394d
rakshitogra94 <rakshit@logica11y.com>
2026-01-25 18:28:42 -0500

    Code change for arrays 25/01/2026

---
+++ vite-project/src/arrays.js
@@ -0,0 +1,45 @@
+const myArr = [0,1,2,3,4,5]
+
+const myHeroes = ["shaktiman","nagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+//console.log(myArr[1])
+
+// myArr.push(6)
+
+// myArr.push(7);
+
+// myArr.pop();
+
+// myArr.unshift(9);
+
+// myArr.shift();
+
+//console.log(myArr);
+
+// console.log(myArr.includes(9))
+
+// console.log(myArr.indexOf(9))
+
+// const newArr = myArr.join();
+
+// console.log(myArr);
+
+// console.log(newArr);
+
+const myNArr1 = myArr.slice(1,3)
+
+console.log("A", myArr);
+
+console.log("B", myNArr1);
+
+const myNArr2 = myArr.splice(1,3)
+
+console.log("C", myArr);
+
+console.log("D", myNArr2);
+
+
+
+
